6c6a576865ac19f8492a6c066563dd282fecb7b9
anil0906 <asharmaanil@gmail.com>
2016-08-23 13:49:50 +1000

    added server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use('/', express.static(path.join(__dirname)));
+app.get('/*', function( req, res ) {
+  res.status(200).sendFile(path.resolve(__dirname, 'index.html'));
+})
+app.listen(8000,function() {
+  console.log("Server started at 8000 port");
+})
